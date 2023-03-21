879aaebe8618bbbdcb320099604500fc85db9562
miniymay <awdry@naver.com>
2023-03-21 14:52:33 -0400

    add datasets

--- allergy_classification/get_auc.py
+++ allergy_classification/get_auc.py
@@ -6,14 +6,28 @@ allergy_list = ["milk", "egg", "peanut"]
 data_type_list = ["_original", "_imputed"]
 
 for data_type in data_type_list :
-	result_df = pd.DataFrame()
-	for allergy in allergy_list :
-		result_list = []
-		for i in range(1, 6) :
-			label_file = "label_group_" + str(i) + "_" + allergy + data_type + ".csv"
-			pred_file = "prediction_group_" + str(i) + "_" + allergy + data_type + ".csv"
-			label = pd.read_csv(label_file, header = None)
-			pred = pd.read_csv(pred_file, header = None)
-			result_list.append(roc_auc_score(label, pred))
-		result_df[allergy] = result_list
-	result_df.to_csv("classification_auc_result" + data_type + ".csv", mode = "w", index = False)
+	final_milk_df = pd.DataFrame()
+	final_egg_df = pd.DataFrame()
+	final_peanut_df = pd.DataFrame()
+	for v in range(1,6) :
+		dirname = "./v" + str(v) + "/"
+		result_df = pd.DataFrame()
+		for allergy in allergy_list :
+			result_list = []
+			for i in range(1, 6) :
+				label_file = dirname + "label_group_" + str(i) + "_" + allergy + data_type + ".csv"
+				pred_file = dirname + "prediction_group_" + str(i) + "_" + allergy + data_type + ".csv"
+				label = pd.read_csv(label_file, header = None)
+				pred = pd.read_csv(pred_file, header = None)
+				result_list.append(roc_auc_score(label, pred))
+			result_df[allergy] = result_list
+		#result_df.to_csv(dirname + "classification_auc_result" + data_type + ".csv", mode = "w", index = False)
+		final_milk_df["Experiment" + str(v)] = result_df["milk"]
+		final_egg_df["Experiment" + str(v)] = result_df["egg"]
+		final_peanut_df["Experiment" + str(v)] = result_df["peanut"]
+	final_milk_df.loc['avg'] = final_milk_df.mean()
+	final_egg_df.loc['avg'] = final_egg_df.mean()
+	final_peanut_df.loc['avg'] = final_peanut_df.mean()
+	final_milk_df.to_csv("results_milk" + data_type + ".csv", mode = "w", index = True)
+	final_egg_df.to_csv("results_egg" + data_type + ".csv", mode = "w", index = True)
+	final_peanut_df.to_csv("results_peanut" + data_type + ".csv", mode = "w", index = True)
--- allergy_classification/run_allergy_classifier.sh
+++ allergy_classification/run_allergy_classifier.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
-dir="./5cv_dataset/"
+# Change dir as the dataset you want to run
+dir="./5cv_dataset/DeepMicroGen/"
 
-allergy_list="milk egg peanut"
-for allergy in $allergy_list
-do 
-    for i in {1..5}
-    do
+for j in {1..5}
+do
+    mkdir "v"$j
+    allergy_list="milk egg peanut"
+    
+    for allergy in $allergy_list
+    do 
+        for i in {1..5}
+        do
         x_data=$dir"train_group_"$i"_X.csv"
         y_data=$dir"train_group_"$i"_"$allergy"_Y.csv"
         x_test=$dir"test_group_"$i"_X.csv"
         y_test=$dir"test_group_"$i"_"$allergy"_Y.csv"
         python allergy_classifier.py $allergy"_original" $i $x_data $y_data $x_test $y_test
+        done
     done
-done
 
-for allergy in $allergy_list
-do 
-    for i in {1..5}
-    do
+    for allergy in $allergy_list
+    do 
+        for i in {1..5}
+        do
         x_data=$dir"train_additional_group_"$i"_X.csv"
         y_data=$dir"train_additional_group_"$i"_"$allergy"_Y.csv"
         x_test=$dir"test_group_"$i"_X.csv"
         y_test=$dir"test_group_"$i"_"$allergy"_Y.csv"
         python allergy_classifier.py $allergy"_imputed" $i $x_data $y_data $x_test $y_test
+        done
     done
+
+    mv *.csv "v"$j
 done
 
 python get_auc.py
